040693cc0f2790223825f03d52a1fac69b7ee85d
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-11-06 18:57:58 +0100

    add:stake

---
+++ programs/nft_stake/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("HgAsB37QttTnYPWKtQAyGjbUb8eevAygxtJaN8YsWchD");
+
+#[program]
+pub mod nft_stake {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
