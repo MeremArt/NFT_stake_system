d612a431711167b5a588b56e756a6f681907f8dc
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-11-09 13:45:06 +0100

    init_user

--- programs/nft_stake/src/instructions/initialize_user.rs
+++ programs/nft_stake/src/instructions/initialize_user.rs
@@ -1,1 +1,21 @@
 use anchor_lang::prelude::*;
+
+use crate::state::UserAccount;
+
+
+#[derive(Accounts)]
+
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        seeds = [b"user".as_ref(), user.key().as_ref()],
+        bump,
+        space = 8 + UserAccount::INIT_SPACE,
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+pub system_program: Program<'info, System>
+}
